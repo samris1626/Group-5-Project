fd3b608eb7bb4db374ceae0ba88063459aa0ff1c
samris1626 <samris1626@students.ecpi.edu>
2025-10-02 13:01:27 -0400

    Update FP-2: file_opps.cpp
    Added readFile.cpp code to this code. This will be the main one to use.

--- FP-2: file_opps.cpp
+++ FP-2: file_opps.cpp
@@ -5,8 +5,20 @@
 #include "file_ops.h"
 
 void readFile() {
-	std::cout << "Reading from File..." << std::endl;
-	//add file reading code at a TBD time
+	std::ifstream inFile("data.txt."); //input file stream, opens file for reading
+
+  if (!inFile) {    //makes sure the file can be found and opened
+      std::cout << "Error: Could not open file for reading.\n";
+      return;  //exit the function early if the file can't be opened
+  }
+
+  std::string line;  //store each line read from the file
+
+  while (std::getline(inFile, line)) {    //read file line by line until end of file is reached
+      std::cout << line << std::endl;    //print each line to the console
+  }
+
+  inFile.close();    //closes the file when done. This is considered a 'best practice' step
 }
 
 void writeFile() {
