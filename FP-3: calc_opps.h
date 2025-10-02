7687f233de07b8808ef34f2bc9e9226823b205bf
samris1626 <samris1626@students.ecpi.edu>
2025-10-02 12:44:49 -0400

    Update FP-3: calc_opps.h
    Updated calc_ops_h to CALC_OPS_H in lines 4-5

--- FP-3: calc_opps.h
+++ FP-3: calc_opps.h
@@ -1,8 +1,8 @@
 //Class: SDC255 - Agile and Scrum
 //Name: Samantha Riser, Broc Salvanti, Venisha Smith
 
-#ifndef calc_ops_h
-#define calc_ops_h
+#ifndef CALC_OPS_H
+#define CALC_OPS_H
 
 void performCalculation();
 
