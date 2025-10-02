357fff6d8b3b44d6aeed2cbcac37582ba2866201
samris1626 <samris1626@students.ecpi.edu>
2025-10-02 11:48:54 -0400

    Create FP-2: file_opps.h

---
+++ FP-2: file_opps.h
@@ -0,0 +1,11 @@
+//Class: SDC255 - Agile and Scrum
+//Name: Samantha Riser, Broc Salvanti, Venisha Smith
+
+//file_ops.h
+#ifndef FILE_OPS.H
+#define FILE_OPS.H
+
+void readFile();
+void writeFile();
+
+#endif
